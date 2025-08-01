446716fc4fe95de0febe5b7ab2e396e040a5cdad
Ingrid64f <ingridjaramillo848@gmail.com>
2025-07-31 22:29:44 -0500

    Repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,30 @@
+//El alert muestra una ventana emegerte en el navegador
+//alert("Holis tengo hambre") ;
+//Prompt muestra una caja para ingresar informacion desde el navegador
+//prompt("Dime tu nombre") ; 
+
+//Variable declarada con let
+
+let personaje ="Demian"
+
+//Crear variable puntaje y asignar valor de 0
+//Los valores numericos van sin comillas
+let puntaje = 0
+//Crear variable shots y asignar valor 3
+let shots = 3
+     recarga = 2
+     
+ console.log(" Total de balas:",shots + recarga) 
+ 
+ //El usuario capturo 4 patos
+puntaje += 4 
+ //El usuario gasto 2 balas    
+shots -= 2 
+ //El usuario recargo su arma 
+ recarga += 1
+recarga++ //++ permite sumar de a uno 
+ 
+
+ console.log("puntaje:",puntaje) 
+ console.log("shots:",shots) 
+ console.log("recarga:",recarga)
