60fb17450ffafc95a2e9f016d5ac4ed114279608
Ingrid64f <ingridjaramillo848@gmail.com>
2025-07-31 22:05:16 -0500

    Ejercicio terminado

---
+++ 2_operadores_comparacion_logicos/app.js
@@ -0,0 +1,50 @@
+1 //Boolean(...)
+console.log( "5" == "5", "5" == "5" ) //true
+console.log( "5" === "5", "5" === "5" ) //true
+console.log( 5 === "5", 5 === "5" ) //false
+console.log( 5 == "5", 5 == "5" ) //true
+console.log( 5 === (3+2), "5" === (3+2) ) //false
+console.log( 5 === 5 , 5 === 5 ) //true
+
+//Concatenar ( + )
+console.log( 5 + "3" ) //53
+console.log( 5 * "3" ) //15
+console.log( 5 + 3 ) //8
+
+//Pida dos numeros al usuario y sumelos
+let num1 = Number(prompt('Dame num 1')) //"5"
+let num2 = Number(prompt('Dame num 3')) //"3"
+let resultado= num1+num2
+alert( "El resultado de su suma es " + resultado );
+
+// Ejercicios para practicar 
+
+// Pedir dos números al usuario usando prompt
+let numero1 = Number (prompt( "Dame un numero" + numero1))
+let numero2 = Number (prompt( "Dame un segundo numero" + numero2))
+
+ console.log ("Su valor A es:" + numero1)
+ console.log ("Su valor B es:" + numero2)
+
+    // Realizar las operaciones 
+    let suma = valorA + valorB;
+    let resta = valorA - valorB;
+    let multiplicacion = valorA * valorB;
+    let division = valorA / valorB;
+/* 
+    let suma= numero1 + numero2
+    console.log("El resultado de tu Suma:"+ numero1+ numero2 + suma)
+
+    let resta= numeroo1 - numero2
+    console.log("El resultado de tu Resta"+ numero1 + numero2 +resta)
+
+    let Multiplicacion= numero1 - numero2
+    console.log("El resultado de tu Multiplicacion"+ multiplicacion)
+
+    let Division= numero1 - numero2
+    console.log("Division"+ division) */
+
+    //Solucion del ejercicio concatenado
+
+    console.log(`Tu numero es ${numerin1}`)
+     
